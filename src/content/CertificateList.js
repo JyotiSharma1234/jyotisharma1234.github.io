c94a58c868ffc72eec4d6fa8026b4d29e090d4c0
JYOTI SHARMA <sjyoti4321@gmail.com>
2021-05-31 12:19:58 +0530

    Certificate List

---
+++ src/content/CertificateList.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import JQueryUdemy from '../../images/certificates/jquery_udemy.jpg'
+import JSBasicHackerRank from '../../images/certificates/javascript_basic_hackerrank.png'
+import ReactBasicHackerRank from '../../images/certificates/react_basic_hackerrank.png'
+import ProblemSolvingBasicHackerRank from '../../images/certificates/problem_solving_hackerrank.png'
+import ProblemSolvingIntermediateHackerRank from '../../images/certificates/problem_solving_hackerrank_intermediate.png'
+import PatOnBackSept2019 from '../../images/certificates/patonbacksept2019.jpg'
+import JavaBasicHackerrank from '../../images/certificates/java_basic_hackerrank.png'
+import AFScholarshipCertificate from '../../images/certificates/AFS.jpg'
+
+let certificatesList = [
+    {
+        index: 0,
+        path: JQueryUdemy,
+        src: JQueryUdemy,
+        title: 'JQuery Udemy',
+        description: ''
+    },
+    {
+        index: 1,
+        path: JSBasicHackerRank,
+        src: JSBasicHackerRank,
+        title: 'JavaScript Basic Hackerrank',
+        description: 'JavaScript (Basic) from Hackerrank'
+    },
+    {
+        index: 2,
+        path: ProblemSolvingBasicHackerRank,
+        src: ProblemSolvingBasicHackerRank,
+        title: 'Problem Solving Basic HackerRank',
+        description: 'Problem Solving (Basic) from HackerRank'
+    },
+    {
+        index: 3,
+        path: ProblemSolvingIntermediateHackerRank,
+        src: ProblemSolvingIntermediateHackerRank,
+        title: 'Problem Solving Intermediate HackerRank',
+        description: 'Problem Solving (Intermediate) from HackerRank'
+    },
+    {
+        index: 4,
+        path: ReactBasicHackerRank,
+        src: ReactBasicHackerRank,
+        title: 'React Basic HackerRank',
+        description: 'React (Basic) from HackerRank'
+    },
+    {
+        index: 5,
+        path: PatOnBackSept2019,
+        src: PatOnBackSept2019,
+        title: 'Pat On Back (Sept 2019)',
+        description: 'Pat On Back (Sept 2019) from Promobi Technology Pvt. ltd.'
+    },
+    {
+        index: 6,
+        path: JavaBasicHackerrank,
+        src: JavaBasicHackerrank,
+        title: 'Java Basic',
+        description: 'Java (Basic) from Hackerrank'
+    },
+    {
+        index: 7,
+        path: AFScholarshipCertificate,
+        src: AFScholarshipCertificate,
+        title: 'Java Basic',
+        description: 'Java (Basic) from Hackerrank'
+    },
+]
+
+export default certificatesList;
